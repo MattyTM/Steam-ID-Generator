07d583aeae7422c9daa32e5a053cb2ea9a651f61
Matias <52473745+MattyTM@users.noreply.github.com>
2020-09-13 14:22:53 -0300

    Updated

--- IDCracker.py
+++ IDCracker.py
@@ -5,7 +5,7 @@
 import random
 import string
 import pathlib
-import requests, os, threading, sys, time, random, ctypes, webbrowser,re, hashlib, datetime, os.path, tkinter
+import requests, os, threading, sys, time, random, ctypes, webbrowser,re, hashlib, datetime, os.path
 import colorama
 from colorama import Fore, init, Back, Style
 from datetime import date
@@ -14,7 +14,7 @@ os.system("title [STEAM] ID Generator - Matty#8952")
 def Spinner():
 	l = ['|', '/', '-', '\\']
 	for i in l+l+l:
-		sys.stdout.write('\r' + '[*] Loading... '+i)
+		sys.stdout.write('\r' + Style.BRIGHT + Fore.YELLOW + Back.BLACK +'[*] Loading... '+i)
 		sys.stdout.flush()
 		time.sleep(0.2)
 
@@ -47,29 +47,34 @@ print(Style.BRIGHT + Fore.WHITE + Back.BLACK +"                                 
 print(Style.BRIGHT + Fore.WHITE + Back.BLACK +"                                         [3] "+Fore.BLUE+"Exit")
 print(Style.BRIGHT + Fore.WHITE + Back.BLACK +"                         ["+Style.BRIGHT + Fore.BLUE + Back.BLACK +"+"+Style.BRIGHT + Fore.WHITE + Back.BLACK +"]"+ Style.BRIGHT + Fore.BLUE + Back.BLACK +"-------------------------------------------------------"+ Style.BRIGHT + Fore.WHITE + Back.BLACK +"["+ Style.BRIGHT + Fore.BLUE + Back.BLACK +"+"+ Style.BRIGHT + Fore.WHITE + Back.BLACK +"]")
 opcion = input("\nChoice: ")
+
 if opcion =='1':
 	os.system("cls")
 	print(banner)
-	cantidad = input("\nHow many steam ID's: \n")
+	cantidad = input("\nHow many steam ID's: ")
+	lenght = int(input("\nLenght of the IDs: "))
+	lenght- 1
 	while int(count)<int(cantidad):
-		Generated = random.choice(string.ascii_uppercase).upper()+''.join(random.choice(string.ascii_uppercase + string.digits).upper() for _ in range(2))
+		lenght - 1
+		Generated = ''.join(random.choice(string.ascii_uppercase + string.digits).upper() for _ in range(lenght))
 		f= open("Generated IDs"+"/"+str("Generated")+str("")+".txt","a")
 		f.write(Generated+"\n")
 		print(Style.BRIGHT + Fore.BLUE + Back.BLACK +"Generated ID: "+ Fore.RESET + Generated)
 		count+=1
-		if int(count)==int(cantidad):
-			print("\n" + Style.BRIGHT + Fore.CYAN + Back.BLACK +"Id's have been generated successfully!")
-			print(Style.BRIGHT + Fore.BLUE + Back.BLACK +"Generated ID's saved in Generated.txt")
-			input(Style.BRIGHT + Fore.RED + Back.BLACK +"\nPress enter to exit")
-			os.system("cls")
-			print(Style.BRIGHT + Fore.WHITE + Back.BLACK +"                         ["+Style.BRIGHT + Fore.BLUE + Back.BLACK +"+"+Style.BRIGHT + Fore.WHITE + Back.BLACK +"]"+ Style.BRIGHT + Fore.BLUE + Back.BLACK +"-------------------------------------------------------"+ Style.BRIGHT + Fore.WHITE + Back.BLACK +"["+ Style.BRIGHT + Fore.BLUE + Back.BLACK +"+"+ Style.BRIGHT + Fore.WHITE + Back.BLACK +"]")
-			print(Style.BRIGHT + Fore.RED + Back.BLACK +"                                                   Closing!")
-			print(Style.BRIGHT + Fore.GREEN + Back.BLACK +"                                               Have a good day :D")
-			print(Style.BRIGHT + Fore.WHITE + Back.BLACK +"                         ["+Style.BRIGHT + Fore.BLUE + Back.BLACK +"+"+Style.BRIGHT + Fore.WHITE + Back.BLACK +"]"+ Style.BRIGHT + Fore.BLUE + Back.BLACK +"-------------------------------------------------------"+ Style.BRIGHT + Fore.WHITE + Back.BLACK +"["+ Style.BRIGHT + Fore.BLUE + Back.BLACK +"+"+ Style.BRIGHT + Fore.WHITE + Back.BLACK +"]")
-			time.sleep(2)
-			sys.exit()
-			pass
-	pass
+pass
+
+if int(count)==int(cantidad):
+	print("\n" + Style.BRIGHT + Fore.CYAN + Back.BLACK +"Id's have been generated successfully!")
+	print(Style.BRIGHT + Fore.BLUE + Back.BLACK +"Generated ID's saved in Generated.txt")
+	input(Style.BRIGHT + Fore.RED + Back.BLACK +"\nPress enter to exit")
+	os.system("cls")
+	print(Style.BRIGHT + Fore.WHITE + Back.BLACK +"                         ["+Style.BRIGHT + Fore.BLUE + Back.BLACK +"+"+Style.BRIGHT + Fore.WHITE + Back.BLACK +"]"+ Style.BRIGHT + Fore.BLUE + Back.BLACK +"-------------------------------------------------------"+ Style.BRIGHT + Fore.WHITE + Back.BLACK +"["+ Style.BRIGHT + Fore.BLUE + Back.BLACK +"+"+ Style.BRIGHT + Fore.WHITE + Back.BLACK +"]")
+	print(Style.BRIGHT + Fore.RED + Back.BLACK +"                                                   Closing!")
+	print(Style.BRIGHT + Fore.GREEN + Back.BLACK +"                                               Have a good day :D")
+	print(Style.BRIGHT + Fore.WHITE + Back.BLACK +"                         ["+Style.BRIGHT + Fore.BLUE + Back.BLACK +"+"+Style.BRIGHT + Fore.WHITE + Back.BLACK +"]"+ Style.BRIGHT + Fore.BLUE + Back.BLACK +"-------------------------------------------------------"+ Style.BRIGHT + Fore.WHITE + Back.BLACK +"["+ Style.BRIGHT + Fore.BLUE + Back.BLACK +"+"+ Style.BRIGHT + Fore.WHITE + Back.BLACK +"]")
+	time.sleep(2)
+	sys.exit()
+pass
 
 if opcion =='2':
 	os.system("cls")
@@ -88,6 +93,7 @@ if opcion =='2':
 	time.sleep(2)
 	sys.exit()
 	pass
+
 if opcion =='3':
 	os.system("cls")
 	print(Style.BRIGHT + Fore.WHITE + Back.BLACK +"                         ["+Style.BRIGHT + Fore.BLUE + Back.BLACK +"+"+Style.BRIGHT + Fore.WHITE + Back.BLACK +"]"+ Style.BRIGHT + Fore.BLUE + Back.BLACK +"-------------------------------------------------------"+ Style.BRIGHT + Fore.WHITE + Back.BLACK +"["+ Style.BRIGHT + Fore.BLUE + Back.BLACK +"+"+ Style.BRIGHT + Fore.WHITE + Back.BLACK +"]")
@@ -96,4 +102,4 @@ if opcion =='3':
 	print(Style.BRIGHT + Fore.WHITE + Back.BLACK +"                         ["+Style.BRIGHT + Fore.BLUE + Back.BLACK +"+"+Style.BRIGHT + Fore.WHITE + Back.BLACK +"]"+ Style.BRIGHT + Fore.BLUE + Back.BLACK +"-------------------------------------------------------"+ Style.BRIGHT + Fore.WHITE + Back.BLACK +"["+ Style.BRIGHT + Fore.BLUE + Back.BLACK +"+"+ Style.BRIGHT + Fore.WHITE + Back.BLACK +"]")
 	time.sleep(2)
 	sys.exit()
-pass		
+	pass		
